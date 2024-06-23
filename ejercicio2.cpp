2b598ef0350ac49e78f562cf872a57406da9a455
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-06-23 12:08:57 -0500

    Actualiza ejercicio2: agrega validación números positivos

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -12,6 +12,10 @@ int main(){
 	cout<<"Ingrese un numero: ";cin>>n1;
 	cout<<"Ingrese otro numero: ";cin>>n2;
 	
+	if(n1<0||n2<0){
+		cout<<"No se permiten numeros negativos\n";
+	}
+	else{
 	c=pesi(n1,n2);
 
 	if(c>1){
@@ -20,6 +24,7 @@ int main(){
 	else{
 		cout<<"Son PESI\n";
 	}
+	}
 	return 0;
 }
 
@@ -27,9 +32,9 @@ int pesi(int a, int b){
 	int c=0,i=1;
 	while(i<=a&&i<=b){
 		if(a%i==0&&b%i==0){
-			c=c+1;
+			c++;
 		}
-		i=i+1;
+		i++;
 	}
 	return (c);
 }
